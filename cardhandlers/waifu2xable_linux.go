3ffe06d27ffac39db1b163e8f6c6a76fbf2c64a8
ninjawarrior1337 <t.rothled@gmail.com>
2020-04-26 15:26:10 -0700

    fix imports

--- cardhandlers/waifu2xable_linux.go
+++ cardhandlers/waifu2xable_linux.go
@@ -1,5 +1,9 @@
 package cardhandlers
 
+import (
+	"os/exec"
+)
+
 func prepareWaifu2xCommand(cmd *exec.Cmd, inputPath, outputPath string) {
 	var waifu2xArgs = []string{"-i", inputPath, "-o", outputPath, "--noise-level", "3", "--scale-ratio", "2"}
 	waifu2xCmd = exec.Command("/usr/bin/waifu2x-converter-cpp", waifu2xArgs...)
