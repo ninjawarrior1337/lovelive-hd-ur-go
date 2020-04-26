dd79c85b2d85a98b7be8663ad17e71fee9d9560c
ninjawarrior1337 <t.rothled@gmail.com>
2020-04-26 15:28:46 -0700

    hhhhhhh

--- cardhandlers/waifu2xable_linux.go
+++ cardhandlers/waifu2xable_linux.go
@@ -6,5 +6,5 @@ import (
 
 func prepareWaifu2xCommand(cmd *exec.Cmd, inputPath, outputPath string) {
 	var waifu2xArgs = []string{"-i", inputPath, "-o", outputPath, "--noise-level", "3", "--scale-ratio", "2"}
-	waifu2xCmd = exec.Command("/usr/bin/waifu2x-converter-cpp", waifu2xArgs...)
+	cmd = exec.Command("/usr/bin/waifu2x-converter-cpp", waifu2xArgs...)
 }
